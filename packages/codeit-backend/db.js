66490221639487bb403c59417dad83aa2957f09d
alqudah1 <alqudah@sheridancollege.ca>
2025-12-16 22:35:46 -0500

    Fix Lightsail DB endpoint

--- packages/codeit-backend/db.js
+++ packages/codeit-backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'ls-44f538763910ead9dcd8cb51164a1961379d7f1b.cf06qk4sweo5.ca-central-1.rds.amazonaws.com',
-  user: 'elearning',
-  password: 'YOUR_DB_PASSWORD',
-  database: 'elearning',
-  port: 3306,
+  host: 'ls-44f538763910ead9dcd8cb51164a1961379d7f1b.cf06qk4sweo5.ca-central-1.rds.amazonaws.com', 
+  user: 'elearning',                                
+  password: 'Mustafa2003',                            
+  database: 'elearning',  
+  port: 3306,                   
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -24,4 +24,3 @@ const pool = mysql.createPool({
 })();
 
 module.exports = pool;
-
