9052643d35b421454074e7ee2f554f39ac95af95
alqudah1 <alqudah@sheridancollege.ca>
2025-12-16 22:05:21 -0500

    Use env vars for DB connection

--- packages/codeit-backend/db.js
+++ packages/codeit-backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'us-east-1.rds.amazonaws.com', 
-  user: 'adm',                                
-  password: 'Team42*',                            
-  database: 'elearning',                      
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'elearning',
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
