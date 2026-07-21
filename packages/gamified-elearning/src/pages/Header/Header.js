22785e6193e362ea5b14bf3d428b52e8f4ee6b53
Mustafa AlQudah <alqudah@sheridancollege.ca>
2026-07-21 23:52:52 +0000

    refactor(header): make the CodeIt brand readable

--- packages/gamified-elearning/src/pages/Header/Header.js
+++ packages/gamified-elearning/src/pages/Header/Header.js
@@ -65,7 +65,7 @@ export default function Header() {
     <header className="site-header">
       <div className="site-header__inner">
         <Link to="/" className="site-header__logo" aria-label="CodeIt home">
-          <img src="/brand/CodeItRG.svg" alt="CodeIt" className="site-header__logo-img" />
+          <span className="site-header__wordmark" aria-hidden="true">Code<strong>It</strong></span>
         </Link>
 
         <button
