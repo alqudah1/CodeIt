a2668c6bec3cb9f57a00ab61a8ee1859c27d4c14
Mustafa AlQudah <alqudah@sheridancollege.ca>
2026-07-27 21:41:14 +0000

    fix(brand): preserve complete original logo artwork

--- packages/gamified-elearning/src/components/BrandLogo/BrandLogo.js
+++ packages/gamified-elearning/src/components/BrandLogo/BrandLogo.js
@@ -4,7 +4,7 @@ import "./BrandLogo.css";
 export default function BrandLogo({ className = "", alt = "CodeIt" }) {
   return (
     <span className={["brand-logo", className].filter(Boolean).join(" ")}>
-      <img src="/brand/LogoForSM.png" alt={alt} />
+      <img src="/brand/codeit-logo-trimmed.png" alt={alt} />
     </span>
   );
 }
